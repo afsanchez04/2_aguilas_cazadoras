f89b474d3ab71b7d00e40ab6bbcbc91d3ab4a625
Mason_2003 <azaz0909123567@gmail.com>
2026-05-05 11:18:54 -0500

    Scripts in objects
    Scripts were added to the characters for the game to function.

---
+++ aguilas_cazadoras_game/Assets/scripts/PlayerThrow.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerThrow : MonoBehaviour
+{
+    public Bomb bomb;
+
+    public GameObject luigi;
+    public GameObject peach;
+    public GameObject yoshi;
+
+    void Update()
+    {
+        // solo si el jugador tiene la bomba
+        if (bomb.currentHolder != gameObject) return;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            bomb.PassBomb(luigi);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            bomb.PassBomb(peach);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            bomb.PassBomb(yoshi);
+        }
+    }
+}
